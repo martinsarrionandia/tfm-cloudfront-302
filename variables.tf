f1389d72b74e3f7cfcafb89face238954153ea9f
Martin Sarrionandia <martin@sarrionandia.co.uk>
2024-12-04 16:29:49 +0000

    First draft

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "domain" {
+  description = "Source domain"
+  type        = string
+}
+
+variable "hostname" {
+  description = "Source hostname"
+  type        = string
+}
+
+variable "target_url" {
+  description = "Target of redirect"
+  type        = string
+}
